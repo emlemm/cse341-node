808aaf420a96dad2aa58ab08bf205d0f6088480f
emlemmon <77080516+emlemmon@users.noreply.github.com>
2025-10-11 21:46:37 -0600

    W04 assignment - api documentation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/swagger', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use(cors()) //Handles all CORS headers
     .use(express.json())
     .use((req, res, next) => {
